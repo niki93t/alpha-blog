ee742f760c61d80338a7ce2596db3174dc451d3f
Nikola Adzic <niki93t@gmail.com>
2018-02-01 19:31:42 +0100

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
       flash[:success] = "Article was successfuly updated"
       redirect_to article_path(@article)
@@ -48,7 +47,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def set_article
